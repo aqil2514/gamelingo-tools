8b20df7d4f97bd008ac6e2fb4ecaaa02c28674d6
aqil2514 <muhamadaqil383@gmail.com>
2023-12-16 21:16:16 +0700

    not found fixed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,9 @@ export default function NotFound() {
   return (
     <div className={DIV_MAIN_STYLE + " py-20"}>
       <p className="font-poppins font-semibold text-base lg:text-2xl text-white mx-8 mt-4">Halaman belum dibuat atau tidak tersedia</p>
-      <button onClick={() => router.back} className="block mx-auto text-center font-mclaren text-slate-200 bg-yellow-600 px-4 py-2">Kembali ke halaman sebelumnya</button>
+      <button onClick={() => router.back()} className="block mx-auto text-center font-mclaren text-slate-200 bg-yellow-600 px-4 py-2">
+        Kembali ke halaman sebelumnya
+      </button>
     </div>
   );
 }
