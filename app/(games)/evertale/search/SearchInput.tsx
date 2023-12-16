f0eef757dbbdc79ed35b47e70c476f00bbba2657
aqil2514 <muhamadaqil383@gmail.com>
2023-12-16 21:00:38 +0700

    search fixed

--- app/(games)/evertale/search/SearchInput.tsx
+++ app/(games)/evertale/search/SearchInput.tsx
@@ -2,7 +2,7 @@ import { Search } from "react-bootstrap-icons";
 
 export default function SearchInput() {
   return (
-    <form className="my-auto mx-auto w-full md:w-1/3">
+    <form action="/evertale/search" className="my-auto mx-auto w-full md:w-1/3">
       <div className="relative">
         <input type="text" name="s" id="s" placeholder="Cari..." className="w-full font-poppins font-bold px-4 rounded-xl" />
         <div className="absolute right-2 top-1">
