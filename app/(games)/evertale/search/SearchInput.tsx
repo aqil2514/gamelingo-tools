c7837fd9e769fddac8c2e8ef7ea747033189322d
aqil2514 <muhamadaqil383@gmail.com>
2023-12-25 17:53:58 +0700

    search fix

--- app/(games)/evertale/search/SearchInput.tsx
+++ app/(games)/evertale/search/SearchInput.tsx
@@ -1,15 +1,17 @@
-import { Search } from "react-bootstrap-icons";
+"use client";
+import { usePathname } from "next/navigation";
 
 export default function SearchInput({ keyword, setKeyword }: React.ComponentState) {
+  const pathName = usePathname();
+
   return (
     <form action="/evertale/search" className="my-auto mx-auto w-full md:w-1/3">
       <div className="relative">
-        <input type="text" name="s" value={keyword} onChange={(e) => setKeyword(e.target.value)} id="s" placeholder="Cari..." className="w-full font-poppins font-bold px-4 rounded-xl" />
-        <div className="absolute right-2 top-1">
-          <button type="submit">
-            <Search />
-          </button>
-        </div>
+        {pathName === "/evertale/chars" ? (
+          <input type="text" name="s" id="s" placeholder="Cari..." className="w-full font-poppins font-bold px-4 rounded-xl" />
+        ) : (
+          <input type="text" name="s" value={keyword} onChange={(e) => setKeyword(e.target.value)} id="s" placeholder="Cari..." className="w-full font-poppins font-bold px-4 rounded-xl" />
+        )}
       </div>
     </form>
   );
--- app/(games)/evertale/search/main.tsx
+++ app/(games)/evertale/search/main.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import axios from "axios";
 import Result from "./Result";
 import SearchInput from "./SearchInput";
-import { useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import useSWR from "swr";
 
 const fetcher = (...args: Parameters<typeof fetch>) => fetch(...args).then((res) => res.json());
@@ -13,6 +11,14 @@ export default function Main() {
   const [keyword, setKeyword] = React.useState("");
   const URL = `/api/gamelingo/evertale/search?s=${keyword}`;
   const { data, isLoading } = useSWR(URL, fetcher, { keepPreviousData: true });
+
+  if (!keyword)
+    return (
+      <>
+        <SearchInput keyword={keyword} setKeyword={setKeyword} />
+        <p className="font-poppins font-semibold text-base lg:text-2xl text-white mx-8 mt-4">Masukkan Keyword</p>
+      </>
+    );
   return (
     <>
       <SearchInput keyword={keyword} setKeyword={setKeyword} />
