004f487d9789369c3a732276e95c50afd94fd3c9
Muhamad Aqil Maulana <128574244+aqil2514@users.noreply.github.com>
2024-02-11 03:23:41 +0000

    fix error

--- src/app/api/file/route.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { NextApiResponse } from "next";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest, res: NextApiResponse) {
-    console.log(res);
-res.json({msg:"ok"})
-}
