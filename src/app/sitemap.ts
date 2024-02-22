1de86bee68667c2e933d49f36a86045c84279d40
aqil2514 <muhamadaqil383@gmail.com>
2024-02-22 17:34:53 +0700

    sitemap fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,9 +5,15 @@ const baseUrl = "https://gamelingo-tools.vercel.app";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const allPost = (await Post.find()) as General.PostDocument[];
+  const getUrl = (post: General.PostDocument): string => {
+    if (post.game.name === "Evertale" && post.game.topic === "Character") return `${baseUrl}/evertale/chars/${post.content}`;
+    else if (post.game.name === "Evertale" && post.game.topic === "Weapon") return `${baseUrl}/evertale/weapons/${post.content}`;
+
+    return baseUrl;
+  };
   return [
     ...allPost.map((post) => ({
-      url: `${baseUrl}/${post.game.name.toLowerCase().replaceAll(" ", "-")}/${post.game.topic.toLowerCase()}/${post.content}`,
+      url: getUrl(post),
       lastModified: new Date(),
     })),
   ];
