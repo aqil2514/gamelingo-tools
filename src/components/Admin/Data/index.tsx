34ad450e9ca251d42d6984fc501db6bab862d911
aqil2514 <muhamadaqil383@gmail.com>
2024-03-01 09:38:52 +0700

    protected route fixed

--- src/components/Admin/Data/index.tsx
+++ src/components/Admin/Data/index.tsx
@@ -3,12 +3,12 @@ import EvertaleData from "./Evertale";
 
 export default function AdminData({ field, subfield, user }: { field: string; subfield: string; user: Account.User }) {
   if (field === "account") {
-    if (user.role !== "Admin") return <p className="text-white mx-4 font-poppins font-bold">Maaf, data sensitif. Aksi dibatasi</p>;
+    if (user.role !== "General Admin") return <p className="text-white mx-4 font-poppins font-bold">Maaf, data sensitif. Aksi dibatasi</p>;
 
     return <AccountData field={field} subfield={subfield} />;
   }
   if (field === "evertale") {
-    if (user.role !== "Admin of Evertale") return <p className="text-white mx-4 font-poppins font-bold">Maaf, anda belum mendapat izin untuk melihat data game ini</p>;
+    if (user.role !== "Admin of Evertale" && user.role === "General Admin") return <p className="text-white mx-4 font-poppins font-bold">Maaf, anda belum mendapat izin untuk melihat data game ini</p>;
 
     return <EvertaleData field={field} subfield={subfield} />;
   }
