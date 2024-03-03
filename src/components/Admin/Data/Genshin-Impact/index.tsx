7eacbe38947d6e136454861770ae010f191cb3d6
aqil2514 <muhamadaqil383@gmail.com>
2024-03-03 09:47:05 +0700

    artifact data ui

--- src/components/Admin/Data/Genshin-Impact/index.tsx
+++ src/components/Admin/Data/Genshin-Impact/index.tsx
@@ -6,6 +6,7 @@ import { Route } from "next";
 import useSWR from "swr";
 import MaterialData from "./MaterialData";
 import { useState } from "react";
+import ArtifactData from "./ArtifactData";
 
 interface GenshinImpactDataProps {
   field: General.AdminQuery["field"];
@@ -22,4 +23,5 @@ export default function GenshinImpactData({ field, subfield }: GenshinImpactData
   if (error) return <Error />;
 
   if (subfield === "Material") return <MaterialData data={data.data} lang={lang} setLang={setLang} />;
+  if (subfield === "Artifact") return <ArtifactData data={data.data} lang={lang} setLang={setLang} />;
 }
