ad46b3b0c68f4d9e7be34b3f7babfe64fefb1d61
aqil2514 <muhamadaqil383@gmail.com>
2024-03-07 23:16:42 +0700

    back

--- src/components/Input/Textarea/index.tsx
+++ src/components/Input/Textarea/index.tsx
@@ -1,27 +1,24 @@
 import clsx from "clsx";
 import React from "react";
 
-interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement>{
-    forId:string;
-    label:string;
+interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  forId: string;
+  label: string;
 }
 
-export enum TextareaStyle{
-    variant_1 = "w-full h-[100px] block  my-4 rounded-xl p-4 text-zinc-950 text-base font-bold font-poppins",
+export enum TextareaStyle {
+  variant_1 = "w-full h-[100px] block  my-4 rounded-xl p-4 text-zinc-950 text-base font-bold font-poppins",
 }
 
-export default function Textarea({forId, className, label, ...props}: TextareaProps){
-    return(
-        <>
-        <label htmlFor={forId} className="text-white font-bold">
-          {label} :
-        </label>
-        <textarea
-          className={clsx(TextareaStyle, className)}
-          name="setValue-2"
-          id={forId}
-          {...props}
-        ></textarea>
-        </>
-    )
+//Edit later
+
+export default function Textarea({ forId, className, label, ...props }: TextareaProps) {
+  return (
+    <>
+      <label htmlFor={forId} className="text-white font-bold">
+        {label} :
+      </label>
+      <textarea className={clsx(TextareaStyle, className)} name="setValue-2" id={forId} {...props}></textarea>
+    </>
+  );
 }
