8be0826bc920865cf4f6c147b5ebd91549827a48
aqil2514 <muhamadaqil383@gmail.com>
2024-03-09 14:35:20 +0700

    charimage test

--- src/components/Content/Write/Evertale/Components/CharacterImage/index.tsx
+++ src/components/Content/Write/Evertale/Components/CharacterImage/index.tsx
@@ -1,5 +1,6 @@
 import Button, { VariantClass } from "@/components/Input/Button";
-import React, { useState } from "react";
+import Image from "next/image";
+import React, { useRef, useState } from "react";
 import { Images } from "react-bootstrap-icons";
 
 interface FileNamesState {
@@ -17,6 +18,8 @@ interface PreviewLinkState {
 export default function CharacterImage() {
   const [fileNames, setFileNames] = useState<FileNamesState>({} as FileNamesState);
   const [previewLink, setPreviewLink] = useState<PreviewLinkState>({} as PreviewLinkState);
+  const imagesRef = useRef<HTMLInputElement | null>(null);
+
   function changeHandler(e: React.ChangeEvent<HTMLInputElement>) {
     const target = e.target as HTMLInputElement;
     if (!target.files || target.files.length === 0) return;
@@ -26,7 +29,8 @@ export default function CharacterImage() {
     const file2 = files.find((f) => f.name.includes("02"));
     const file3 = files.find((f) => f.name.includes("03"));
 
-    if (!file1) throw new Error("Error: Minimal harus ada satu gambar");
+    if (!file1) throw new Error("Error: Gambar pertama harus ada. Pastikan namanya mengandung '01'");
+
     const fileName1 = file1.name;
     const fileName2 = file2 ? file2.name : "";
     const fileName3 = file3 ? file3.name : "";
@@ -37,17 +41,52 @@ export default function CharacterImage() {
     setFileNames({ fileName1, fileName2, fileName3 });
     setPreviewLink({ link1, link2, link3 });
   }
+
+  function deleteHandler() {
+    if (imagesRef.current) {
+      imagesRef.current.value = "";
+      imagesRef.current.files = null;
+      setFileNames({} as FileNamesState);
+      setPreviewLink({} as PreviewLinkState);
+    }
+  }
+
   return (
     <div>
       <label htmlFor="characterImages">
-        <Button className={VariantClass.fetch}>
+        <Button className={VariantClass.fetch} type="button">
           <span className="mx-2">
             <Images />
           </span>{" "}
           Upload Images
         </Button>
-        <input type="file" id="characterImages" name="characterImages" className="hidden" accept=".png, .webp" multiple onChange={changeHandler} />
+        <input type="file" ref={imagesRef} id="characterImages" name="characterImages" className="hidden" accept=".png, .webp" multiple onChange={changeHandler} />
       </label>
+      {fileNames.fileName1 && (
+        <div>
+          <Button className={VariantClass.danger} type="button" onClick={deleteHandler}>
+            Hapus semua
+          </Button>
+          <div className="my-4 flex justify-center gap-4">
+            <div>
+              <Image src={previewLink.link1} alt={fileNames.fileName1} height={128} width={128} className="w-auto block aspect-square" />
+              <p className="font-bold text-white font-poppins my-4 text-center">{fileNames.fileName1}</p>
+            </div>
+            {previewLink.link2 && fileNames.fileName2 && (
+              <div>
+                <Image src={previewLink.link2} alt={fileNames.fileName2} height={128} width={128} className="w-auto block aspect-square" />
+                <p className="font-bold text-white font-poppins my-4 text-center">{fileNames.fileName2}</p>
+              </div>
+            )}
+            {previewLink.link3 && fileNames.fileName3 && (
+              <div>
+                <Image src={previewLink.link3} alt={fileNames.fileName3} height={128} width={128} className="w-auto block aspect-square" />
+                <p className="font-bold text-white font-poppins my-4 text-center">{fileNames.fileName3}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
