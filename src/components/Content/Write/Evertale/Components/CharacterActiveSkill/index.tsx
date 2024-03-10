cac8d4f1e560e732d7fcc7f5a11595558b553ce6
aqil2514 <muhamadaqil383@gmail.com>
2024-03-10 15:33:23 +0700

    add field adjusment

--- src/components/Content/Write/Evertale/Components/CharacterActiveSkill/index.tsx
+++ src/components/Content/Write/Evertale/Components/CharacterActiveSkill/index.tsx
@@ -27,6 +27,7 @@ export default function CharacterActiveSkill() {
     <div>
       <h5 className="text-white text-xl underline text-center font-bold font-poppins my-4">Character Active Skill</h5>
       <div>
+        <h6 className="text-white text-xl underline font-bold font-poppins my-4">Active Skill 1</h6>
         <TextField forId={`active-skill-name-${index}`} label="Skill Name" variant="default-variant-1" name={`active-skill-name-${1}`} />
         <TypeSkill index={index} />
         <div className="grid grid-cols-3 gap-4">
