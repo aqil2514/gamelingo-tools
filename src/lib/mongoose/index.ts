4db3436ec515f511856b650eb6985200dd67eee5
aqil2514 <muhamadaqil383@gmail.com>
2024-03-12 02:36:45 +0700

    config options

--- src/lib/mongoose/index.ts
+++ src/lib/mongoose/index.ts
@@ -1,8 +1,11 @@
 import mongoose, { ConnectOptions, Connection } from "mongoose";
 
 const config: ConnectOptions = {
-  maxPoolSize: 10,
+  maxPoolSize: 1,
   minPoolSize: 1,
+  socketTimeoutMS: 10000,
+  serverSelectionTimeoutMS: 10000,
+  maxIdleTimeMS: 10000,
 };
 
 // Membuat koneksi evertale
