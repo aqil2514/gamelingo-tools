c81d2c216cddf7c793546000bb1179dd887af0b6
aqil2514 <muhamadaqil383@gmail.com>
2024-03-14 23:24:24 +0700

    ui adjusment

--- src/components/general/Search/Input/index.tsx
+++ src/components/general/Search/Input/index.tsx
@@ -3,11 +3,23 @@
 import { usePathname } from "next/navigation";
 import React from "react";
 
-const SearchInput = ({ field, isInPage, setKeyword, keyword }: { field: "evertale" | "genshinImpact" | "mlbb"; isInPage: boolean; keyword?: string; setKeyword?: React.Dispatch<React.SetStateAction<string>> }) => {
+interface SearchInputProps {
+  field: "evertale" | "genshinImpact" | "mlbb";
+  isInPage: boolean;
+  keyword?: string;
+  setKeyword?: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SearchInput = ({
+  field,
+  isInPage,
+  setKeyword,
+  keyword,
+}: SearchInputProps) => {
   const pathName = usePathname();
   const pathes = pathName.split("/");
 
-  const category = pathes[pathes.length - 1];
+  const category = pathes[2];
   let url;
   if (!isInPage && !setKeyword) {
     if (field === "evertale") url = `/evertale/search`;
@@ -15,14 +27,26 @@ const SearchInput = ({ field, isInPage, setKeyword, keyword }: { field: "evertal
       <form action={url} className="my-auto mx-auto w-full">
         <div className="relative">
           <input type="hidden" name="category" id="category" value={category} />
-          <input type="text" name="s" placeholder={`Cari ${category}...`} className="w-full font-poppins font-bold px-4 rounded-xl" />
+          <input
+            type="text"
+            name="s"
+            placeholder={`Cari ${category}...`}
+            className="w-full font-poppins font-bold px-4 rounded-xl"
+          />
         </div>
       </form>
     );
   } else if (setKeyword)
     return (
       <>
-        <input type="text" name="s" value={keyword} onChange={(e) => setKeyword(e.target.value)} placeholder="Cari..." className="w-full md:w-1/2 mx-auto block font-poppins font-bold px-4 rounded-xl" />
+        <input
+          type="text"
+          name="s"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          placeholder="Cari..."
+          className="w-full md:w-1/2 mx-auto block font-poppins font-bold px-4 rounded-xl"
+        />
       </>
     );
 };
