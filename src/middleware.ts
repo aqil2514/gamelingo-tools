ae54bdfd21aecbd179f6f125c7603dc606794c90
aqil2514 <muhamadaqil383@gmail.com>
2024-04-05 03:09:15 +0700

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,22 @@
-console.log("test")
+import { NextRequest } from "next/server";
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+
+export function middleware(req: NextRequest) {
+    req.headers.set("Accept-Language", "en");
+    const lang = req.headers.get("Accept-Language");
+
+    console.log(lang)
+//   let headers = { "accept-language": "id,en;q=0.5" };
+//   let languages = new Negotiator({ headers }).languages();
+//   let locales = ["id", "en"];
+//   let defaultLocale = "id";
+
+//   const test = match(languages, locales, defaultLocale);
+
+//   console.log(test)
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
